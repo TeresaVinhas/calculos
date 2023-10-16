ed4f91491b2c489fb539e0dff0b2754c13a07265
TeresaV <35941004+TeresaVinhas@users.noreply.github.com>
2023-10-16 17:21:01 +0100

    Criar a classe Conversões e mover método
    O Método ConverterTemperatura()foi removido da classe aritmética e colocado na classe Conversoes uma vez que se implementa uma operação que não se adequa à classe.

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -9,46 +9,23 @@ namespace Calculos
     /// <summary>
     /// Implementa operações aritmeticas
     /// </summary>
-    internal class Aritmetica
-    {
-        /// <summary>
-        /// Tipo de conversão de temperatura a executar.
-        /// </summary>
-        public enum ConversaoTemperatura
-        { 
-        Nulo = 0,
-        CelsiusFahrenheit,
-        FahrenheitCelsius
-        }
-     /// <summary>
+    internal static class Aritmetica
+    { 
+    /// <summary>
     /// Operação Soma
     /// </summary>
     /// <returns> Retorna a soma de dois numeros</returns>
-        public int Somar(int x, int y)
+        public static int Somar(int x, int y)
         {
             return x + y;
         }
         /// <summary>
         /// Operação de Subtração
         /// </summary>
-       /// <returns> Retorna o resultado da subtração de dois numeros</returns>
-        public int Subtrair(int x, int y)
+        /// <returns> Retorna o resultado da subtração de dois numeros</returns>
+        public static int Subtrair(int x, int y)
         {
             return x - y;
-        }
-
-        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
-        {
-            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
-            {
-                return (temperatura * 1.8000 + 32);
-            }
-            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
-            {
-                return ((temperatura - 32) / 1.8000);
-            }
-            return -1;
-        }
-
+        }   
     }
 }
